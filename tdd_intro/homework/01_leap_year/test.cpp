e58691ad6de2f0c92b221cd7ba6aff36b3be520d
Dmitry Buksha <dmitry7393@gmail.com>
2019-05-23 18:17:14 +0300

    Refactoring

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -18,12 +18,8 @@ bool isLeapYear(int year)
 {
     if (year % 4 == 0)
     {
-        if (year % 100 == 0)
+        if (year % 100 == 0 && year % 400 != 0)
         {
-            if (year % 400 == 0)
-            {
-                return true;
-            }
             return false;
         }
         return true;
