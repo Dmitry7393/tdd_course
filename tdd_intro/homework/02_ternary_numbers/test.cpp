0481ca1fa97c86579416dedecd17a25b38dcd238
Dmitry Buksha <dmitry7393@gmail.com>
2019-05-31 15:39:08 +0300

    Implemented green test

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -20,6 +20,12 @@ If your language provides a method in the standard library to perform the conver
 
 int convertTernaryNumberToDecimal(const std::string& ternaryNumber)
 {
+    if (ternaryNumber.length() == 2)
+    {
+        int firstSymbol = ternaryNumber[0] - '0';
+        int secondSymbol = ternaryNumber[1] - '0';
+        return (firstSymbol * pow(3, 1) + secondSymbol * pow(3, 0));
+    }
     int n = ternaryNumber[0] - '0';
     return n * pow(3, 0);
 }
